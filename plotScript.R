d0db8fe391ef57e5533206c9803aee6a8bbf1b86
Kevin <kevintjchou@gmail.com>
2016-10-18 16:28:34 -0500

    Edited plotScript to include all variables and added SongCSV file

--- plotScript.R
+++ plotScript.R
@@ -1,18 +1,53 @@
 # Performs linear regression on Year v. all the other variables.
-# The linear regression function is only written for Year v. Tempo.  Change "Tempo" to
-# to other variables
 # Plots the linear model with a scatterplot and boxplot.
 
 
 # Linear regression
-reg<-lm(Year~Tempo,dat=dta)
+regTempo<-lm(Year~Tempo,data=dta)
+regDuration<-lm(Year~Duration,data=dta)
+regTimeSignature<-lm(Year~TimeSignature,data=dta)
+regKeySignature<-lm(Year~KeySignature,data=dta)
+regArtistLatitude<-lm(Year~ArtistLatitude,data=dta)
+regArtistLongitude<-lm(Year~ArtistLongitude,data=dta)
+regStartFadeOut<-lm(Year~StartFadeOut,data=dta)
+regEndFadeIn<-lm(Year~EndFadeIn,data=dta)
 
 # Plot data
 with(dta,plot(Tempo,Year))
-abline(coef(reg)) # draw trend line using coefficients of reg
+abline(coef(regTempo)) # draw trend line using coefficients of the linear regression
+
+with(dta,plot(Duration,Year))
+abline(coef(regDuration))
+
+with(dta,plot(TimeSignature,Year))
+abline(coef(regTimeSignature))
+
+with(dta,plot(KeySignature,Year))
+abline(coef(regKeySignature))
+
+with(dta,plot(ArtistLatitude,Year))
+abline(coef(regArtistLatitude))
+
+with(dta,plot(ArtistLongitude,Year))
+abline(coef(regArtistLongitude))
+
+with(dta,plot(StartFadeOut,Year))
+abline(coef(StartFadeOut))
+
+with(dta,plot(EndFadeIn,Year))
+abline(coef(EndFadeIn))
 
 # Plot data with log
 with(dta,plot(log(Tempo),Year))
+with(dta,plot(log(Duration),Year))
+with(dta,plot(log(TimeSignature),Year))
+with(dta,plot(log(KeySignature),Year))
+with(dta,plot(log(ArtistLatitude),Year))
+with(dta,plot(log(ArtistLongitude),Year))
+with(dta,plot(log(StartFadeOut),Year))
+with(dta,plot(log(EndFadeIn),Year))
 
 # Boxplot
 with(dta,boxplot(Year~Tempo))
+with(dta,boxplot(Year~KeySignature))
+with(dta,boxplot(Year~TimeSignature))
