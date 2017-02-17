bed1ed5cd3a4824d50637441ba4f4144f7dcbdae
ardabney <adabney@stat.tamu.edu>
2017-02-17 10:30:50 -0600

    Add files via upload

--- randomForest.R
+++ randomForest.R
@@ -2,12 +2,15 @@ library(randomForest)
 library(tree)
 require(xgboost)
 
+region_dta_selected$region <- factor(region_dta_selected$region)
+
 set.seed(1)
 samp <- sample(nrow(region_dta_selected), 0.6 * nrow(region_dta_selected))
 train <- region_dta_selected[samp,]
 test <- region_dta_selected[-samp,]
-region_dta_selected$region <- NULL
-tree.region <- tree(region~., data=train, mtry=7)
-model <- randomForest(region~., data=train)
+
+#region_dta_selected$region <- NULL
+tree.region <- tree(region~., data=train)#, mtry=7)
+model <- randomForest(region~., data=train, mtry = 7)
 boost.region <- gbm(region~., data=train)
 yhat.boost <- predict(boost.region, newdata=test)
